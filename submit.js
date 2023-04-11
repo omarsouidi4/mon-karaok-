60ca653e69a77c787cba742a377dc533e1b7819b
Omar Souidi <omar.souidi@epfedu.fr>
2023-04-11 12:42:26 +0200

    Ajout de la BDD firebase

--- submit.js
+++ submit.js
@@ -1,9 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvqDFe7LdPEzse8pC8R9ZstrFVzO_0bZE",
+  authDomain: "cantobar-7fba0.firebaseapp.com",
+  projectId: "cantobar-7fba0",
+  storageBucket: "cantobar-7fba0.appspot.com",
+  messagingSenderId: "532252931763",
+  appId: "1:532252931763:web:969157dc8d576f4c50e2c6",
+  measurementId: "G-TP5LRMBZ8T"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
 const form = document.getElementById("song-form");
 const artistInput = document.getElementById("artist");
 const songInput = document.getElementById("song");
 const tableInput = document.getElementById("table");
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", async function (event) {
     event.preventDefault();
     const artist = artistInput.value;
     const song = songInput.value;
@@ -13,15 +31,15 @@ form.addEventListener("submit", function (event) {
         artist: artist,
         song: song,
         table: table,
-        timestamp: new Date(),
+        timestamp: Date.now(),
     };
 
-    storeSongRequest(request);
+    try {
+        const docRef = await addDoc(collection(db, "songRequests"), request);
+        console.log("Song request submitted with ID:", docRef.id);
+    } catch (error) {
+        console.error("Error submitting song request:", error);
+    }
+
     form.reset();
 });
-
-function storeSongRequest(request) {
-    const existingRequests = JSON.parse(localStorage.getItem("songRequests")) || [];
-    existingRequests.push(request);
-    localStorage.setItem("songRequests", JSON.stringify(existingRequests));
-}
